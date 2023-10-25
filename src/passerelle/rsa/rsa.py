0e1ccbfdb800673e6b7522dc418dcc220dbcbe9d
theoahga <clere.theo@gmail.com>
2023-10-25 18:52:38 +0200

    rsa keys creation

---
+++ src/passerelle/rsa/rsa.py
@@ -0,0 +1,32 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+# Generate an RSA key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+
+# Serialize the private key
+private_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+# Create a public key from the private key
+public_key = private_key.public_key()
+
+# Serialize the public key
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+
+print(private_pem.decode("UTF-8"))
+print(public_pem.decode("UTF-8"))
+
+

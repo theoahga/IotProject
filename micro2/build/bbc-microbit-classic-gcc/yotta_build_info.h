98297b338faadaeefd9f2a59babf92411e0bc2fe
Maxime DUBOIS <maxime.dubois.1@tpopt11.cpe.lan>
2023-10-26 15:34:43 +0200

    cryptage ok

--- micro2/build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ micro2/build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -3,9 +3,9 @@
 // yotta build info, #include YOTTA_BUILD_INFO_HEADER to access
 #define YOTTA_BUILD_YEAR 2023 // UTC year
 #define YOTTA_BUILD_MONTH 10 // UTC month 1-12
-#define YOTTA_BUILD_DAY 25 // UTC day 1-31
-#define YOTTA_BUILD_HOUR 10 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 4 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 44 // UTC second 0-61
-#define YOTTA_BUILD_UUID c60df0e1-a99e-4ffc-b7f1-a57969e0db31 // unique random UUID for each build
+#define YOTTA_BUILD_DAY 26 // UTC day 1-31
+#define YOTTA_BUILD_HOUR 13 // UTC hour 0-24
+#define YOTTA_BUILD_MINUTE 33 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 47 // UTC second 0-61
+#define YOTTA_BUILD_UUID c05d0391-7048-442e-81a7-1c5d8e368d80 // unique random UUID for each build
 #endif // ndef __YOTTA_BUILD_INFO_H__
--- micro2/source/main.cpp
+++ micro2/source/main.cpp
@@ -3,18 +3,67 @@
 #include "ssd1306.h"
 #include "bme280.h"
 #include "tsl256x.h"
+#include <stdio.h>
+#include <string.h>
+#include <cctype>
 
 MicroBit uBit;
 MicroBitI2C i2c(I2C_SDA0,I2C_SCL0);
 MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_DIGITAL_OUT);
 ssd1306 screen(&uBit, &i2c, &P0);
-MicroBitSerial serial(USBTX, USBRX);
 
-ManagedString CODE = "DMST";
-ManagedString ESCAPE = "|";
+char CODE[] = "DMST";
 
-ManagedString ORDER = "LPHT";
-int datalignestart = 4;
+char ORDER[] = "LPHT";
+
+#define MAX_TEXT_LENGTH 20
+
+char encryptedText[MAX_TEXT_LENGTH];
+char decryptedText[MAX_TEXT_LENGTH];
+
+char key[] = "bcdefghijklmnopqrstuvwxyz";
+
+
+char* encrypt(const char* text, char* key) {
+    int textLength = strlen(text);
+
+    for (int i = 0; i < textLength; i++) {
+        char currentChar = text[i];
+        if (isalpha(currentChar)) {
+            char isUpperCase = isupper(currentChar);
+            currentChar = tolower(currentChar);
+            int charIndex = currentChar - 'a';
+            char encryptedChar = isUpperCase ? toupper(key[charIndex]) : key[charIndex];
+            encryptedText[i] = encryptedChar;
+        } else {
+            encryptedText[i] = currentChar;
+        }
+    }
+    encryptedText[textLength] = '\0';
+    return encryptedText;
+}
+
+char* decrypt(const char* encryptedText, char* key) {
+    int textLength = strlen(encryptedText);
+
+    for (int i = 0; i < textLength; i++) {
+        char currentChar = encryptedText[i];
+        if (isalpha(currentChar)) {
+            char isUpperCase = isupper(currentChar);
+            currentChar = tolower(currentChar);
+            char* charIndex = strchr(key, currentChar);
+            int index = charIndex - key;
+            char decryptedChar = index + 'a';
+            decryptedChar = isUpperCase ? toupper(decryptedChar) : decryptedChar;
+            decryptedText[i] = decryptedChar;
+        } else {
+            decryptedText[i] = currentChar;
+        }
+    }
+    decryptedText[textLength] = '\0';
+    return decryptedText;
+}
+
 
 bool check_dsmt(ManagedString s){
     return s.substring(0,sizeof(CODE)) == CODE;
@@ -24,6 +73,18 @@ void send_RF(ManagedString s){
     ManagedString tosend = CODE + s;
     uBit.radio.datagram.send(tosend);
     screen.display_line(3,0,tosend.toCharArray());
+    screen.update_screen();
+}
+
+void send_encrypt_RF(ManagedString s){
+    char* encryptedText = encrypt(const_cast<char*>(s.toCharArray()), key);
+
+    screen.display_line(1,0,encryptedText);
+
+    char* decryptedText = decrypt(encryptedText, key);
+
+    screen.display_line(2,0,decryptedText);
+
     screen.update_screen();
 }
 
@@ -40,24 +101,14 @@ void onData(MicroBitEvent)
 
     screen.update_screen();
 
-    if (s == "1")
-        uBit.display.print("A");
-
-    if (s == "2")
-        uBit.display.print("B");
-
     if (check_dsmt(s)){
         screen.display_line(2,0,decode_RF(s).toCharArray());
         screen.update_screen();
-    }
-    
-
-    printf(s.toCharArray());
-    serial.send("\n");
-
+    }    
 }
 
 void data(bme280 *bme,tsl256x *tsl, ManagedString order ){
+
     uint32_t pressure = 0;
     int32_t temp = 0;
     uint16_t humidite = 0;
@@ -67,9 +118,9 @@ void data(bme280 *bme,tsl256x *tsl, ManagedString order ){
     uint32_t lux = 0;
 
     bme->sensor_read(&pressure, &temp, &humidite);
-    int tmp = bme->compensate_temperature(temp);
-    int pres = bme->compensate_pressure(pressure)/100;
-    int hum = bme->compensate_humidity(humidite);
+    short tmp = bme->compensate_temperature(temp);
+    short pres = bme->compensate_pressure(pressure)/100;
+    short hum = bme->compensate_humidity(humidite);
 
     tsl->sensor_read(&comb, &ir, &lux);
 
@@ -78,9 +129,15 @@ void data(bme280 *bme,tsl256x *tsl, ManagedString order ){
     ManagedString humdys = "Humi:" + ManagedString(hum/100) + "." + ManagedString(tmp%100)+" rH";
     ManagedString presdys = "Pres:" + ManagedString(pres)+" hPa";
     ManagedString luxdys = "Lux:" + ManagedString((int)lux);
+    uint64_t currentTime = uBit.systemTime();
+    
+    ManagedString d = "T:"+ ManagedString(tmp/100) + "." + (tmp > 0 ? ManagedString(tmp%100): ManagedString((-tmp)%100))+";L:"+ ManagedString((int)lux) +";" + ManagedString((int)currentTime);
     
     screen.display_line(3,0, order.toCharArray());
-    
+    screen.display_line(4,0, d.toCharArray());
+    /*
+    short datalignestart = 4;
+
     for (int i=0; i<order.length(); i++){
         screen.display_line(i + datalignestart,0, "              ");
         switch(order.toCharArray()[i]) {
@@ -99,7 +156,16 @@ void data(bme280 *bme,tsl256x *tsl, ManagedString order ){
         default:
             break;
         }
-    }
+    }*/
+
+    send_RF(tempdys);
+    /*
+    send_RF(humdys);
+    send_RF(presdys);
+    */
+    send_RF(luxdys);
+
+    send_encrypt_RF("test");
     screen.update_screen();
 }
 
@@ -123,6 +189,7 @@ int main()
     
     while(1)
     {
+        /*
         if (uBit.buttonA.isPressed())
         {
 
@@ -136,11 +203,9 @@ int main()
             send_RF("Hello World ! ");
 
         }
+        */
         data(&bme, &tsl, ORDER);
-        
-        
-        
-        uBit.sleep(100);
+        uBit.sleep(1000);
     }
 
     release_fiber();

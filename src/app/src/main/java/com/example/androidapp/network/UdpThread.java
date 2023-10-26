30ea99cb252c7aec326b1b407077f30623687e2e
sspina69 <sandro.spina.pro@gmail.com>
2023-10-26 14:25:07 +0200

    clean project

---
+++ src/app/src/main/java/com/example/androidapp/network/UdpThread.java
@@ -0,0 +1,34 @@
+package com.example.androidapp.network;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.concurrent.BlockingQueue;
+
+public class UdpThread extends Thread{
+    private BlockingQueue<String> queue;
+    private String ip;
+    private String port;
+
+    public UdpThread(BlockingQueue<String> queue, String ip, String port){
+        this.queue = queue;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = queue.take();
+            DatagramSocket UDPSocket = new DatagramSocket();
+            InetAddress address = InetAddress.getByName(this.ip);
+            byte[] data = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(data,data.length, address, Integer.parseInt(this.port));
+            UDPSocket.send(packet);
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
